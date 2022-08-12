52dd7747b4afedd70c5e6976e2f26a07080d4764
Mariana Macedo <mariana.macedo@visto.global>
2022-08-12 10:41:05 -0300

    colocando checkbox pra melhorar a ui kkk

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { XMLParser } from "fast-xml-parser";
 import * as zip from "@zip.js/zip.js";
 import { useEffect, useState } from "react";
+import { styled } from "@mui/material/styles";
 import {
   Table,
   TableHead,
@@ -12,13 +13,21 @@ import {
   TableContainer,
   Input,
   Button,
+  Checkbox,
 } from "@mui/material";
 import "moment";
 import moment from "moment";
 
+const CustomizedTableRow = styled(TableRow)`
+  & .Mui-selected {
+    background-color: "red";
+  }
+`;
+
 function App() {
   const [asset, setAsset] = useState(null);
   const [objetos, setObjetos] = useState([]);
+  const [selectedRows, setSelectedRows] = useState([]);
 
   useEffect(() => {
     if (!asset) return;
@@ -49,7 +58,10 @@ function App() {
 
     //parse xmls
     const parser = new XMLParser();
-    const jObj = xmls.map((xml, i) => ({ ...parser.parse(xml), id: ids[i] }));
+    const jObj = xmls.map((xml, i) => {
+      console.log(parser.parse(xml), i);
+      return { ...parser.parse(xml), id: ids[i] };
+    });
 
     setObjetos(jObj);
 
@@ -58,8 +70,10 @@ function App() {
   };
 
   const rows = objetos.map((objeto) => {
+    console.log(objeto);
     const { ide, dest, total } = objeto.CFe.infCFe;
     const [y1, y2, y3, y4, m1, m2, d1, d2] = ide.dEmi.toString();
+    const cpfLength = dest.CPF?.toString().length;
     return {
       id: objeto.id,
       document: ide.nCFe,
@@ -67,7 +81,7 @@ function App() {
         "DD/MM/YYYY"
       ),
       totalValue: total.vCFe,
-      cpf: dest.CPF,
+      cpf: cpfLength === 10 ? `0${dest.CPF}` : dest.CPF,
     };
   });
 
@@ -85,11 +99,29 @@ function App() {
     </TableCell>
   ));
 
+  const handleOnClick = (event, row) => {
+    const isSelected = selectedRows.indexOf(row) !== -1;
+    console.log(selectedRows.indexOf(row));
+    if (isSelected) {
+      setSelectedRows((prev) => [...prev].filter((it) => row !== it));
+      return;
+    }
+    setSelectedRows((prev) => [...prev, row]);
+  };
+  console.log(selectedRows);
+
   const table = rows.map((row, j) => {
     return (
-      <TableRow key={`tableRow${j}`}>
+      <TableRow
+        key={`tableRow${j}`}
+        selected={selectedRows.indexOf(row) !== -1}
+        onClick={(e) => handleOnClick(e, row)}
+      >
+        <TableCell>
+          <Checkbox onChange={handleOnClick} />
+        </TableCell>
         {cells.map((cell, i) => (
-          <TableCell align="center" key={`${cell.field}${i}`} selected>
+          <TableCell align="center" key={`${cell.field}${i}`}>
             {row[cell.field]}
           </TableCell>
         ))}
@@ -123,7 +155,10 @@ function App() {
             >
               <Table size="small" aria-label="a dense table">
                 <TableHead>
-                  <TableRow>{headCells}</TableRow>
+                  <TableRow>
+                    <TableCell></TableCell>
+                    {headCells}
+                  </TableRow>
                 </TableHead>
                 <TableBody>{table}</TableBody>
               </Table>
